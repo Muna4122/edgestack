7734c5d1a1d38b60d31e7220f6efc1a317602c67
Muna4122 <munachimusochibuzor@gmail.com>
2026-01-23 11:57:32 +0100

    First commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function HomePage () {
+    return (
+    <main>
+      <div>
+       <p className="text-4xl text-blue-500"> Welcome to my home page</p>
+      </div>
+    </main>
+    )
+ }
